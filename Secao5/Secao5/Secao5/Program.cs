302538708a50962411b0812de3593a51cdee6a1a
diegosmteixeira <diegosmteixeira@gmail.com>
2021-01-20 18:14:12 -0300

    Secao 5 - Construtores, palavra this, sobrecarga, encapsulamento

---
+++ Secao5/Secao5/Secao5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Secao5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
