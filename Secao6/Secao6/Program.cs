5b3d84d78b8c91dcc92ffecff17a616ff1ba991a
diegosmteixeira <diegosmteixeira@gmail.com>
2021-01-21 02:26:27 -0300

    Secao 6 - Tipos referência, tipos valor e valores padrão

--- Secao6/Secao6/Program.cs
+++ Secao6/Secao6/Program.cs
@@ -10,6 +10,18 @@ namespace Secao6
     {
         static void Main(string[] args)
         {
+            //Apenas declarar a variável não possibilita que ela seja impressa
+            Point p;
+
+            //Para que permita imprimir a variável é necessário inicializar os valores
+            p.X = 10;
+            p.Y = 20;
+
+            Console.WriteLine(p);
+
+            p = new Point();
+
+            Console.WriteLine(p);
         }
     }
 }
