f8083b3e63aeba8b5fa2ece1380285a504016fb0
diegosmteixeira <diegosmteixeira@gmail.com>
2021-01-21 03:00:01 -0300

    Secao 6 - Garbage Collector, nullable, operador de coalescencia nula

--- Secao6/Secao6/Program.cs
+++ Secao6/Secao6/Program.cs
@@ -10,6 +10,7 @@ namespace Secao6
     {
         static void Main(string[] args)
         {
+            /*
             //Apenas declarar a variável não possibilita que ela seja impressa
             Point p;
 
@@ -22,6 +23,58 @@ namespace Secao6
             p = new Point();
 
             Console.WriteLine(p);
+
+            //Conclusão: Mesmo sendo um tipo struct deve-se inicializar (ou colocando valores, ou fazendo new).
+            */
+
+
+            //Declarando variáveis nulas (de valor opcional):
+
+            Nullable<double> x = null;
+
+            //ou
+            double? y = 10.0;
+
+            //pega o valor do X ou o valor padrão da variável x (no caso, o zero)
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            //diz se dentro da variável existe, ou não, um valor
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+            //pegar valor diretamente
+            /*Console.WriteLine(x.Value);*/ //lança uma exceção se você utilizá-la a partir de um nullable
+            Console.WriteLine(y.Value);
+
+            //testando com condições:
+            if (x.HasValue)
+            {
+                Console.WriteLine(x.Value);
+            }
+            else
+            {
+                Console.WriteLine("X is null");
+            }
+
+            if (y.HasValue)
+            {
+                Console.WriteLine(y.Value);
+            }
+            else
+            {
+                Console.WriteLine("Y is null");
+            }
+
+            //*** Operador de Coalescência Nula ***
+
+            double? z = null;
+
+            // se o valor de Z for nulo, joga em k o valor à direita do ??
+            double k = z ?? 20.0;
+
+            Console.WriteLine(k);
+
         }
     }
 }
