d5ff401bfc1e1eb4cee76f17b66311fdedc6e5b9
diegosmteixeira <diegosmteixeira@gmail.com>
2021-01-21 01:56:51 -0300

    Secao 6 - Comportamento de memória, arrays, listas

---
+++ Secao6/Secao6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Secao6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
