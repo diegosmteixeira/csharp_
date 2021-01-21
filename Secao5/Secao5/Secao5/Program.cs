10b8e489c813e451217bd0462cb061ce4c743651
diegosmteixeira <diegosmteixeira@gmail.com>
2021-01-20 22:28:37 -0300

    Secao5 - Encapsulamento

--- Secao5/Secao5/Secao5/Program.cs
+++ Secao5/Secao5/Secao5/Program.cs
@@ -8,36 +8,13 @@ namespace Secao5
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Programa para adicionar ou remover produtos de um Estoque: ");
-            Console.WriteLine("------------------------------");
-            Console.WriteLine("Entre com os dados do produto: ");
-            Console.WriteLine("Nome: ");
-            string nome = Console.ReadLine();
-            Console.WriteLine("Preço: ");
-            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            /*
-            Console.WriteLine("Quantidade no estoque: ");
-            int quantidade = int.Parse(Console.ReadLine());
-            */
-            Produto p = new Produto(nome, preco);
+            Produto p = new Produto("TV", 500.00, 10);
 
-            Console.WriteLine("Dados do produto: " + p);
-            Console.WriteLine();
+            p.SetNome("TV 4K");
 
-            Console.Write("Digite o número de produtos a ser adicionado ao Estoque: ");
-            int qte = int.Parse(Console.ReadLine());
-            p.AdicionarProdutos(qte);
+            Console.WriteLine(p.GetNome());
 
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + p);
-            Console.WriteLine();
-
-            Console.Write("Digite o número de produtos a ser removido do Estoque: ");
-            qte = int.Parse(Console.ReadLine());
-            p.RemoverProdutos(qte);
-
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + p);
+            //Note que quantidade e preco não podem ser alterados, pois não possuem métodos "Setters"
         }
     }
 }
