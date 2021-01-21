f69169e3fba856c94b3a392a9697058b9459cf9d
diegosmteixeira <diegosmteixeira@gmail.com>
2021-01-20 21:15:27 -0300

    Secao 5 - Construtor para a classe produto

--- Secao5/Secao5/Secao5/Program.cs
+++ Secao5/Secao5/Secao5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Secao5
 {
@@ -6,7 +7,36 @@ namespace Secao5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Programa para adicionar ou remover produtos de um Estoque: ");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.WriteLine("Nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto p = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser adicionado ao Estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser removido do Estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
         }
     }
 }
