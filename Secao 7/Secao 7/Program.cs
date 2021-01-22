155e14882f42fbb34f06be28e481dc26921f5a04
diegosmteixeira <diegosmteixeira@gmail.com>
2021-01-22 13:40:00 -0300

    Secao 7 - DateTime

--- Secao 7/Secao 7/Program.cs
+++ Secao 7/Secao 7/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Secao_7
 {
@@ -6,6 +7,7 @@ namespace Secao_7
     {
         static void Main(string[] args)
         {
+            /*
             //Funções para strings
 
             string original = "abcde FGHIJ ABC abc DEFG   ";
@@ -73,16 +75,64 @@ namespace Secao_7
             Console.Write("LastIndexOf('bc'): " + n2);
             Console.WriteLine();
 
-
-
-
-
-
-
-
-
             Console.WriteLine();
             Console.WriteLine();
+            */
+
+
+            //DateTime
+            DateTime d = DateTime.Now;
+
+            Console.WriteLine("Hora Atual: " + d);
+            Console.WriteLine("Total de Ticks desde a hora zero: " + d.Ticks); 
+
+            //Ano, mes, dia
+            DateTime d1 = new DateTime(2018, 11, 25);
+            Console.WriteLine("ano/mes/dia: " + d1);
+
+            //Ano, mes, dia, hora
+            DateTime d2 = new DateTime(2018, 11, 25, 20, 45, 3);
+            Console.WriteLine("ano/mes/dia hora:minutos:segundos: " + d2);
+
+            //nao aceita milissegundos nesse formato
+            DateTime d3 = new DateTime(2018, 11, 25, 20, 45, 3, 500); // nesse tipo de formatação não aceita milissegundos (logo, o 500 não é impresso)
+            Console.WriteLine("ano/mes/dia hora:minutos:segundos: " + d3 + " (não imprime milissegundos)");
+
+            //Utilizando os builders
+            DateTime d4 = DateTime.Now;
+            DateTime d5 = DateTime.Today;
+            DateTime d6 = DateTime.UtcNow; //horário atual no Greenwich
+
+            Console.WriteLine("Now: " + d4);
+            Console.WriteLine("Today: " + d5);
+            Console.WriteLine("UtcNow: " + d6);
+
+
+            //Converter uma data de string para DateTime
+            DateTime d7 = DateTime.Parse("2000-08-15");
+            DateTime d8 = DateTime.Parse("2000-08-15 13:05:58");
+
+            Console.WriteLine("Converter de string para DateTime: " + d7);
+            Console.WriteLine("Converter de string para DateTime com hora: " + d8);
+
+            //Aceita também converter uma data de string no formato BR
+            DateTime d9 = DateTime.Parse("15/08/2000");
+            DateTime d10 = DateTime.Parse("15/08/2000 13:05:58");
+
+            Console.WriteLine("Converter de string para DateTime(BR): " + d9);
+            Console.WriteLine("Converter de string para DateTime(BR) com hora: " + d10);
+
+
+            //1. ParseExact - quando você quer determinar o formato da data
+            //2. após a data usar uma máscara de formatação no modelo como você deseja formatar ex: "yyyy-MM-dd"
+            //3. após a máscara utilizar o Format Provider (CultureInfo.InvariantCulture)
+
+            DateTime d11 = DateTime.ParseExact("2000-08-15", "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            Console.WriteLine("ParseExact: " + d11);
+
+            DateTime d12 = DateTime.ParseExact("15/08/2000 13:05:58", "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+            Console.WriteLine("ParseExact com hora: " + d12);
+
         }
     }
 }
