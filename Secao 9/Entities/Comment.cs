e449657435383bde0abac05c27b3388f289ccb77
diegosmteixeira <diegosmteixeira@gmail.com>
2021-01-24 16:57:51 -0300

    Secao 9 - StringBuilder

---
+++ Secao 9/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace Secao_9.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        { 
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
--- Secao 9/Program.cs
+++ Secao 9/Program.cs
@@ -43,6 +43,7 @@ namespace Secao_9
              * [Repositories]: Objetos responsaveis por acessar os dados de um banco de dados ou de repositórios de dados;
             */
 
+            /*
             Console.WriteLine("------------------------------------------------");
             Console.WriteLine("Programa para calcular os ganhos de um empregado");
             Console.WriteLine("------------------------------------------------");
@@ -89,7 +90,33 @@ namespace Secao_9
             Console.WriteLine("Name: " + worker.Name);
             Console.WriteLine("Department: " + worker.Department.Name);
             Console.WriteLine("Income for " + monthAndYear + ": " + worker.Income(year, month).ToString("F2", CultureInfo.InvariantCulture));
+            */
 
+            Comment c1 = new Comment("Have a nice trip");
+            Comment c2 = new Comment("Wow that's awesome!");
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2018 13:05:44"),
+                "Traveling to New Zealand",
+                "I'm going to visit this wonderful contry!",
+                12
+                );
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Good night");
+            Comment c4 = new Comment("May the Force be with you");
+            Post p2 = new Post(
+                DateTime.Parse("28/07/2018 23:14:19"),
+                "Good night guys",
+                "See you tomorrow",
+                5
+                );
+
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
         }
     }
 }
