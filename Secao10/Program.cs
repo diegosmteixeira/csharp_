a243d3c37022ce644afefac579a7a002862f64d2
diegosmteixeira <diegosmteixeira@gmail.com>
2021-01-25 01:10:51 -0300

    Secao 10 - Upcasting e Downcasting

--- Secao10/Program.cs
+++ Secao10/Program.cs
@@ -18,6 +18,53 @@ namespace Secao10
                account.Balance = 200.0; //não é possível
             */
 
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500);
+
+            //UPCASTING = Conversão da subclasse para superclasse
+
+            Account acc1 = bacc; // Conversão de um <objeto> BusinessAccount para uma <variável> Account
+            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0); // Conversão de uma instânciação subclass para superclasse
+            Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+
+            //DOWNCASTING = Conversão da superclasse para subclasse
+
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            //é necessário um casting (BusinessAccount) antes, 
+            //pois o compilador não enxerga o objeto que já está associado à acc2 (que é uma BusinessAccount)
+            //apenas enxerga o tipo (Account) da variável
+
+            acc4.Loan(100.0);
+            //acc2.Loan(100.0) não existe, pois o compilador apenas enxerga o tipo Account para acc2
+            //apenas existirá o acc2.Loan se for feito um Downcasting antes.
+
+            Console.WriteLine("BusinessAccount acc5 = (BusinessAccount)acc3; GERA ERRO");
+            //BusinessAccount acc5 = (BusinessAccount)acc3;
+            //***Embora o compilador não declare ERRO em tempo de execução ele acontece***
+            //*BusinessAccount não é compatível com SavingsAccount*
+
+            Console.WriteLine("Downcasting é uma operação insegura");
+
+            //Teste de possibilidade de conversão:
+            if (acc3 is BusinessAccount)
+            {
+                //Essa operação vai falhar, pois acc3 é SavingsAccount
+                BusinessAccount acc6 = (BusinessAccount)acc3;
+                acc6.Loan(200.0);
+                Console.WriteLine("Loan! - Emprestimo feito");
+            }
+
+            if (acc3 is SavingsAccount)
+            {
+                //Essa operação terá êxito e será impressa.
+                SavingsAccount acc6 = (SavingsAccount)acc3;
+                acc6.UpdateBalance();
+                Console.WriteLine("Update do Balance");
+            }
+
+            //Sintaxe alternativa
+
+            //BusinessAccount acc7 = acc3 as BusinessAccount;
         }
     }
 }
