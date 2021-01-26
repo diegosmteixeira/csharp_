a2a16139a4f88f3c2ade889c6b4296c939722f3f
diegosmteixeira <diegosmteixeira@gmail.com>
2021-01-26 17:38:07 -0300

    Secao 13 - Trabalhando com arquivos (File e FileInfo)

---
+++ Secao13/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Secao13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            //A notação do @ serve para não ter que usar \\ para representar 
+            //o caminho do arquivo
+            string sourcePath = @"c:\temp\file1.txt";
+            string targetPath = @"c:\temp\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                //fileInfo.CopyTo(targetPath);
+
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
