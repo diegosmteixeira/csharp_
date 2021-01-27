4da6c356b6c831dcbe242345e35193613e2e574e
diegosmteixeira <diegosmteixeira@gmail.com>
2021-01-27 00:17:33 -0300

    Secao 13 - Trabalhando com arquivos (FileStream e StreamReader)

--- Secao13/Program.cs
+++ Secao13/Program.cs
@@ -6,7 +6,8 @@ namespace Secao13
     class Program
     {
         static void Main(string[] args)
-        {            
+        {
+            /*
             //A notação do @ serve para não ter que usar \\ para representar 
             //o caminho do arquivo
             string sourcePath = @"c:\temp\file1.txt";
@@ -27,6 +28,67 @@ namespace Secao13
             {
                 Console.WriteLine("An error occurred");
                 Console.WriteLine(e.Message);
+            }
+            */
+
+            /*
+            string path = @"c:\temp\file1.txt";
+
+            FileStream fs = null; //stream genérica para gerenciar entrada e saída
+            StreamReader sr = null; //stream particular para você realizar o que quer
+
+            try
+            {
+                //São métodos externos conversando com o sistema operacional
+                //Esse recurso não é gerenciado pelo CLR do .NET
+
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs); //instanciou um objeto StreamReader com o parâmetro fs
+                string line = sr.ReadLine(); //armazena na variável "line" a leitura do objeto StreamReader
+
+                Console.WriteLine(line);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred: ");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                //Como esse recurso não é gerenciado pelo CLR do .NET
+                //é necessário fechar essa Stream independente se deu certo ou não.
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
+            }
+            */
+
+            //Outra maneira é utilizando o File.OpenText
+            string path = @"c:\temp\file1.txt";
+
+            StreamReader sr = null; //stream particular para você realizar o que quer
+
+            try
+            {
+                //São métodos externos conversando com o sistema operacional
+                //Esse recurso não é gerenciado pelo CLR do .NET
+
+                sr = File.OpenText(path); //utilizou a classe file para instanciar objetos de stream 
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine(); //enquanto não chegar no EndOfStream lê a linha e mostra na tela
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred: ");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                //Como esse recurso não é gerenciado pelo CLR do .NET
+                //é necessário fechar essa Stream independente se deu certo ou não.
+                if (sr != null) sr.Close();
             }
         }
     }
