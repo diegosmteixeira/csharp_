f28db5800cbbec027edbb6da587ad997b0216d97
diegosmteixeira <diegosmteixeira@gmail.com>
2021-01-27 22:16:37 -0300

    Secao 14 - Interface, inversão de controle e injeção de dependência)

--- Secao14/Program.cs
+++ Secao14/Program.cs
@@ -25,7 +25,7 @@ namespace Secao14
 
             CarRental carRental = new CarRental(start, finish, new Vehicle(model));
 
-            RentalService rentalService = new RentalService(hour, day);
+            RentalService rentalService = new RentalService(hour, day, new BrazilTaxService());
 
             rentalService.ProcessInvoice(carRental);
 
--- Secao14/Services/RentalService.cs
+++ Secao14/Services/RentalService.cs
@@ -8,12 +8,14 @@ namespace Secao14.Services
         public double PricePerHour { get; private set; }
         public double PricePerDay { get; private set; }
 
-        private BrazilTaxService _brazilTaxService = new BrazilTaxService();
+        //Inversão de controle por meio de injeção de dependência
+        private ITaxService _taxService;
 
-        public RentalService(double pricePerHour, double pricePerDay)
+        public RentalService(double pricePerHour, double pricePerDay, ITaxService taxService)
         {
             PricePerHour = pricePerHour;
             PricePerDay = pricePerDay;
+            _taxService = taxService;
         }
 
         public void ProcessInvoice(CarRental carRental)
@@ -30,7 +32,7 @@ namespace Secao14.Services
                 basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
             }
 
-            double tax = _brazilTaxService.Tax(basicPayment);
+            double tax = _taxService.Tax(basicPayment);
 
             carRental.Invoice = new Invoice(basicPayment, tax);
         }
