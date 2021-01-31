b7311e4ae904dbc99dba50c4f697c69f7e85aa23
diegosmteixeira <diegosmteixeira@gmail.com>
2021-01-31 19:41:24 -0300

    Secao 15 - Generics <T> Tipo T

---
+++ Secao15/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Secao15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tipo <T> Genérico pede que quando você for instanciar você declare o tipo que você vai usar
+            PrintService<string> printService = new PrintService<string>();
+
+            Console.WriteLine("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Digite o {i+1}º valor: ");
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x.ToString());
+            }
+
+            printService.Print();
+            Console.WriteLine();
+            Console.WriteLine("First: " + printService.First());
+        }
+    }
+}
