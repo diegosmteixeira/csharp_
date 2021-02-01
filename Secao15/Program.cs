159a39f6bf06c03fef90528215ac1c9b0bfebe86
diegosmteixeira <diegosmteixeira@gmail.com>
2021-01-31 22:15:15 -0300

    Secao 15 - Restrições de generics (IComparable)

--- Secao15/Program.cs
+++ Secao15/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Secao15
 {
@@ -6,6 +7,7 @@ namespace Secao15
     {
         static void Main(string[] args)
         {
+            /*
             //Tipo <T> Genérico pede que quando você for instanciar você declare o tipo que você vai usar
             PrintService<string> printService = new PrintService<string>();
 
@@ -22,6 +24,26 @@ namespace Secao15
             printService.Print();
             Console.WriteLine();
             Console.WriteLine("First: " + printService.First());
+            */
+
+            List<int> list = new List<int>();
+
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i< n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                list.Add(x);
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            int max = calculationService.Max(list);
+
+            Console.WriteLine("Max: ");
+            Console.WriteLine(max);
+
         }
     }
 }
